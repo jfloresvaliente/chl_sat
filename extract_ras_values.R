15b751d086247463afa417dec0bd1a806fb9bbd8
jfloresvaliente <jorgefloresvaliente@gmail.com>
2018-12-06 01:07:05 -0500

    Minor changes

--- extract_ras_values.R
+++ extract_ras_values.R
@@ -13,9 +13,10 @@ library(mapdata)
 library(imputeTS)
 
 dirpath <- 'D:/Clorofila/crop_Miraflores/'
+umbrales <- c(1,10,15,20)
 
 rasterFiles <- list.files(path = dirpath, pattern = '.*\\.nc', full.names = T, recursive = T)
-ras <- raster(rasterFiles[1]); dim(ras)
+ras <- raster(rasterFiles[1])
 
 rasArray <- array(dim = c(dim(ras)[1] * dim(ras)[2], length(rasterFiles)) ); dim(rasArray)
 for(i in 1:length(rasterFiles)){
@@ -23,19 +24,39 @@ for(i in 1:length(rasterFiles)){
   print(rasterFiles[i])
 }
 
-# # Plot del numero de datos disponibles por pixel
-# dfna <- numeric(length = dim(rasArray)[1])
-# for(i in 1:dim(rasArray)[1]){
-#   dfna[i] <- sum(!is.na(rasArray[i,]))
-# }
-# ras[] <- dfna
+#=============================================================================#
+# PLOT DEL NUMERO DE DATOS DISPONIBLES POR PIXEL
+#=============================================================================#
+dfna <- numeric(length = dim(rasArray)[1])
+for(i in 1:dim(rasArray)[1]){
+  dfna[i] <- sum(!is.na(rasArray[i,]))
+}
+ras[] <- dfna
 # x11()
-# plot(ras)
-# map('worldHires', add=T, fill=T, col='gray')
+png(filename = paste0(dirpath, 'AvailableData.png'), width = 850, 850, res = 120)
+par(lwd = 2)
+plot(ras)
+map('worldHires', add=T, fill=T, col='gray')
+mtext(text = 'Available Data', side = 3, line = -2, adj = .95, font = 2, cex = 2)
+dev.off()
 
-# Interpolacion de las series
+#=============================================================================#
+# PLOT CHL SIN INTERPOLAR
+#=============================================================================#
+dfmean <- apply(rasArray, c(1), mean, na.rm = T)
+ras[] <- dfmean
+
+png(filename = paste0(dirpath, 'MeanChlAlldata.png'), width = 850, 850, res = 120)
+par(lwd = 2)
+plot(ras)
+map('worldHires', add=T, fill=T, col='gray')
+dev.off()
+
+#=============================================================================#
+# INTERPOLACION DE LAS SERIES
+#=============================================================================#
 NAinterp <- rasArray
-minNA <- 2
+minNA <- 10
 for(i in 1:dim(NAinterp)[1]){
   if( sum(!is.na(NAinterp[i,])) > minNA ){
     NAinterp[i,] <- na.interpolation(NAinterp[i,], option = 'stine')
@@ -44,15 +65,21 @@ for(i in 1:dim(NAinterp)[1]){
   }
 }
 
-# df <- apply(NAinterp, c(1), mean, na.rm = T); length(df)
-# ras[] <- df
+#=============================================================================#
+# PLOT CHL INTERP
+#=============================================================================#
+df <- apply(NAinterp, c(1), mean, na.rm = T)
+ras[] <- df
 # x11()
-# plot(ras, main = paste('Minimum Available Data:', minNA))
-# map('worldHires', add=T, fill=T, col='gray')
+png(filename = paste0(dirpath, 'MeanChlAlldataInterp.png'), width = 850, 850, res = 120)
+par(lwd = 2)
+plot(ras)
+map('worldHires', add=T, fill=T, col='gray')
+dev.off()
 
+#=============================================================================#
 # CONTABILIZAR EL NUMERO DE FLORACIONES
-umbrales <- c(1,10,15,20)
-
+#=============================================================================#
 get_num_picks <- function(serie){
   picks <- rle(serie)
   picks <- cbind(picks$lengths, picks$values)
@@ -60,26 +87,32 @@ get_num_picks <- function(serie){
   picks <- dim(picks)[1]
   return(picks)
 }
-# x11()
-# par(mfrow = c(2,2))
-# for(i in umbrales){
-#   umbral <- i
-#   NUMpicks <- NAinterp
-#   NUMpicks[NUMpicks <  umbral] <- 0
-#   NUMpicks[NUMpicks >= umbral] <- 1
-#   
-#   picks_number <- apply(NUMpicks, c(1), get_num_picks)
-#   ras[] <- picks_number
-#   # x11()
-#   plot(ras, main = paste('Umbral:', umbral))
-#   map('worldHires', add=T, fill=T, col='gray')
-# }
 
+#=============================================================================#
+# PLOT FLORACIONES ALLDATA
+#=============================================================================#
+png(filename = paste0(dirpath, 'TotalPicksUmbrales.png'), width = 850, 850, res = 120)
+par(mfrow = c(2,2), mar = c(2.5, 2,1, 2))
+for(i in umbrales){
+  umbral <- i
+  NUMpicks <- NAinterp
+  NUMpicks[NUMpicks <  umbral] <- 0
+  NUMpicks[NUMpicks >= umbral] <- 1
 
+  picks_number <- apply(NUMpicks, c(1), get_num_picks)
+  ras[] <- picks_number
+  # x11()
+  plot(ras)
+  map('worldHires', add=T, fill=T, col='gray')
+  mtext(text = paste('Umbral:', umbral), side = 3, line = -1.5, adj = .95, font = 2)
+}
+dev.off()
+
+#=============================================================================#
+# PLOT PROMEDIO CHL POR PIXEL BY YEAR: 2002 - 2018
+#=============================================================================#
 fechas <- seq(from = as.Date('2002-01-01'), to = as.Date('2002-12-31'), by = 'days')
 fechas <- seq(from = fechas[185], length.out = length(rasterFiles), by = 'days')
-
-# fechas <- seq(from = fechas[185], length.out = 181, by = 'days')
 
 dates <- array(dim = c(length(fechas), 3))
 for(i in 1:length(fechas)){
@@ -96,7 +129,7 @@ for(year in 2002:2018){
   
   pngfile <- paste0(dirpath, year, 'floraciones.png')
   png(filename = pngfile, height = 950, width = 1050, res = 120)
-  par(mfrow = c(2,2))
+  par(mfrow = c(2,2), mar = c(2.5, 2,1, 2))
   for(i in umbrales){
     umbral <- i
     NUMpicks <- yearDF
@@ -106,8 +139,9 @@ for(year in 2002:2018){
     picks_number <- apply(NUMpicks, c(1), get_num_picks)
     ras[] <- picks_number
     # x11()
-    plot(ras, main = paste('Umbral:', umbral, '| YEAR: ', year))
+    plot(ras)
     map('worldHires', add=T, fill=T, col='gray')
+    mtext(text = paste('Umbral:', umbral, '| YEAR: ', year), side = 3, line = -1.5, adj = .95, font = 2)
   }
   dev.off()
   print(pngfile)
